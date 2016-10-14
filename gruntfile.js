6bec5ac4620f718d954bd3f216d97a6a107ecc60
Chris Beard <coebe500@aol.com>
2016-10-14 11:05:25 +0100

    Base files and grunt setup

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+	// node require
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		// create link to package.json items
+    	pkg: grunt.file.readJSON('package.json'),
+
+      // grunt-contrib-compass		
+    	compass: {                  	// Task
+    		dev: {                   	// Target
+				options: {              // Target options
+			    	sassDir: 'components/sass',
+			    	cssDir: 'builds/development/css',
+			    	environment: 'development'
+      			}
+    		}, // close dist
+    		prod: {                    // Another target
+      			options: {
+        			sassDir: 'components/sass',
+        			cssDir: 'builds/production/css'
+      			}
+    		} // close dev
+    	}, // close compass
+
+      // grunt-postcss
+    	postcss: {
+			options: {
+    			map: {
+            inline: false,
+            annotation: 'builds/production/css'
+          },
+          processors: [
+      				require('pixrem')(), // disable if this gives issues with font mixins
+      				require('autoprefixer')({ browsers: ['last 2 versions'] }),
+      				require('cssnano')() // minify the result
+    			]
+  			},
+  			dist: {
+  				src:'builds/production/css/*.css'
+  			}  // postcss options		    
+    	}	// postcss
+
+	}); //initConfig
+
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-postcss');
+	
+	grunt.registerTask('default', ['compass', 'postcss']);
+
+}; // wrapper function
